35858ae3b42f89500b833fb25dfaafc390407cf6
haleydek <hcd09@live.com>
2019-08-30 17:36:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]+/g, "");
+  }
+
+  static titleize(string) {
+    const nonCapWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let stringArr = string.split(" ");
+
+    let capArr = stringArr.map(word => {
+      if (!nonCapWords.includes(word) || stringArr.indexOf(word) === 0) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    })
+
+    return capArr.join(" ");
+  }
 }
