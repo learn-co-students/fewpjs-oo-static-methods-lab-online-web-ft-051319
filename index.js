2f38bc8f6aedfd4eda09e34c1df0e6326b134b66
Kane Koo <kanekoo@gmail.com>
2019-09-11 20:20:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+  static capitalize(phrase) {
+    phrase = phrase.split(' ')
+    for (var i=0, x=phrase.length; i < x; i++) {
+      phrase[i] = phrase[i][0].toUpperCase() + phrase[i].substr(1)
+    }
+    return phrase.join(' ')
+  }
+
+  static sanitize(phrase) {
+      return phrase.replace(/[^0-9a-z-\s\']/gi, '')
+  }
+  static titleize(phrase) {
+    phrase = phrase.split(' ')
+    let stopwords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+
+      for (var i=0, x=phrase.length; i < x; i++) {
+        if (!stopwords.includes(phrase[i]) || i===0) {
+
+          phrase[i] = phrase[i][0].toUpperCase() + phrase[i].substr(1)
+          }
+    }
+    return phrase.join(' ')
+  }
 }
